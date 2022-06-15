7d04e6124d849afd00ebc7d3374fc6c26d16c44a
leehyeryeong <w2108@e-mirim.hs.kr>
2022-06-15 12:00:23 +0900

    js change toggle icon

--- 220608_school cafeteria/JS/main.js
+++ 220608_school cafeteria/JS/main.js
@@ -3,12 +3,16 @@ const toggleMenu = (toggleID, navListID) => {
     //html -> js
     const toggle = document.getElementById(toggleID);
     const navList = document.getElementById(navListID);
+    const toggleIcon = toggle.getElementsByTagName("i")[0];
 
     if(toggle&&navList) {
         //toggle click
         toggle.addEventListener('click', () => {
             //show-hide menu: .show-menu
             navList.classList.toggle('show-menu');
+            //change toggle icon: bx-menu <-> bx-x
+            toggleIcon.classList.toggle('bx-menu');
+            toggleIcon.classList.toggle('bxs-x-square');
         });
     }
 }
