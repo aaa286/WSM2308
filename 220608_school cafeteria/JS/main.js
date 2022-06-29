8ad13aad7b68fd6cad8ab35d4571385e26234e83
leehyeryeong <w2108@e-mirim.hs.kr>
2022-06-29 11:38:34 +0900

    html, css, js addNow()

--- 220608_school cafeteria/JS/main.js
+++ 220608_school cafeteria/JS/main.js
@@ -32,3 +32,37 @@ toggleMenu("nav-toggle", "nav-list");
 //     console.log("Hello World");
 // }
 // say();
+
+const addNow = (mainCardID) => {
+    //html -> js
+    const mainCard = document.getElementById(mainCardID);
+    
+    //지금 몇 시? 다음 식사 -> 몇 번째 카드인지
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    console.log(`${hour}시 ${minute}분`);
+    let minutes = hour * 60 + minute; //현재 시와 분을 통해 분으로 단위 통일
+
+    //지금 식사 -> index
+    //조식 끝: 7:30 -> 1
+    //중식 끝: 13:10 -> 2
+    //석식 끝: 18:10 -> 0
+    let index = 0;
+    if(18*60+10<=minutes) { //18:10 <= now
+        index = 0;
+    } else if(13*60+10<=minutes) { //13:10 <= now
+        index = 2;
+    } else if(7*60+30<=minutes) { //7:30 <= now
+        index = 1;
+    } else {
+        index = 0;
+    }
+    
+    //다음 식사 -> 몇 번째 카드인지
+    let selectedCard = mainCard.getElementsByClassName('card')[index];
+    //now 클래스 추가
+    selectedCard.classList.add('now');
+
+}
+addNow("mainCardID");
